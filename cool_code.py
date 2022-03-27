cd17c8306c50338d540abca19d89e09a1987d6ec
Garth <mortensengarth@hotmail.com>
2022-03-26 20:01:38 -0500

    Add OS and dir agnostic pointer

--- cool_code.py
+++ cool_code.py
@@ -3,19 +3,20 @@
 Created on Fri Mar 25 21:47:33 2022
 
 @author: cool dudez
-
-                  ______              _________     
-_____________________  /  __________________  /____ 
+                  ______              _________
+_____________________  /  __________________  /____
 _  ___/  __ \  __ \_  /   _  ___/  __ \  __  /_  _ \
 / /__ / /_/ / /_/ /  /    / /__ / /_/ / /_/ / /  gm/
-\___/ \____/\____//_/     \___/ \____/\__,_/  \___/ 
-                                                    
-                                  
+\___/ \____/\____//_/     \___/ \____/\__,_/  \___/
+
 """
 
-from pathlib import Path
+import os
 import pandas as pd
 
-filepath = Path("./data/input/OnlineNewsPopularity.csv")
-df = pd.read_csv(filepath)
+# OS- and directory-agnostic pointer
+filename = f"OnlineNewsPopularity.csv"
+directory = os.path.dirname(__file__)
+filepath = os.path.join(directory, "data", "input", filename)
 
+df = pd.read_csv(filepath)
